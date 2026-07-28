698ecef01e0dfb158bc6e9623322af62ab7f43f4
track2000one <track2000one@gmail.com>
2026-07-29 01:30:00 +0300

    Merge additional Qudurat banks with duplicate filtering

--- academy/exam-bank-imported-2026.js
+++ academy/exam-bank-imported-2026.js
@@ -6,14 +6,17 @@
     ...(window.NEON_IMPORTED_QUANT_C || []),
     ...(window.NEON_IMPORTED_VERBAL_A || []),
     ...(window.NEON_IMPORTED_VERBAL_B || []),
-    ...(window.NEON_IMPORTED_READING || [])
+    ...(window.NEON_IMPORTED_READING || []),
+    ...(window.NEON_IMPORTED_NOON_QUANT || []),
+    ...(window.NEON_IMPORTED_NOON_VERBAL_A || []),
+    ...(window.NEON_IMPORTED_NOON_VERBAL_B || [])
   ];
   window.NEON_IMPORTED_EXAM_QUESTIONS_2026 = questions;
   window.NEON_IMPORTED_EXAM_SOURCE_STATS = {
     total: questions.length,
     quant: questions.filter(item => item.subject === 'qudurat-quant').length,
     verbal: questions.filter(item => item.subject === 'qudurat-verbal').length,
-    filesReviewed: 9,
-    exactDuplicateFilesSkipped: 1
+    filesReviewed: 11,
+    exactDuplicateFilesSkipped: 2
   };
 })();
