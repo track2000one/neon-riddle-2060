4b6c14ed5b937abb1ddff058cec0f4bd184d4d36
track2000one <track2000one@gmail.com>
2026-07-29 02:23:02 +0300

    Merge verified visual quantitative questions into exam bank

--- academy/exam-bank-imported-2026.js
+++ academy/exam-bank-imported-2026.js
@@ -9,14 +9,16 @@
     ...(window.NEON_IMPORTED_READING || []),
     ...(window.NEON_IMPORTED_NOON_QUANT || []),
     ...(window.NEON_IMPORTED_NOON_VERBAL_A || []),
-    ...(window.NEON_IMPORTED_NOON_VERBAL_B || [])
+    ...(window.NEON_IMPORTED_NOON_VERBAL_B || []),
+    ...(window.NEON_IMPORTED_VISUAL_QUANT || [])
   ];
   window.NEON_IMPORTED_EXAM_QUESTIONS_2026 = questions;
   window.NEON_IMPORTED_EXAM_SOURCE_STATS = {
     total: questions.length,
     quant: questions.filter(item => item.subject === 'qudurat-quant').length,
     verbal: questions.filter(item => item.subject === 'qudurat-verbal').length,
-    filesReviewed: 11,
+    visualQuant: questions.filter(item => item.subject === 'qudurat-quant' && item.visualId).length,
+    filesReviewed: 12,
     exactDuplicateFilesSkipped: 1,
     duplicateHeavyFilesSkipped: 1
   };
