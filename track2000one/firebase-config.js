e2d497c7d59965ad9f08f7ef31090968e09b1335
track2000one <track2000one@gmail.com>
2026-07-28 20:52:24 +0300

    Add Firebase admin configuration

---
+++ track2000one/firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCTXMacLqU0MgHIKi6C7q9VjURAl0srD8",
+  authDomain: "neon-riddle-2060-admin.firebaseapp.com",
+  projectId: "neon-riddle-2060-admin",
+  storageBucket: "neon-riddle-2060-admin.firebasestorage.app",
+  messagingSenderId: "255301604146",
+  appId: "1:255301604146:web:63451bb582f171423cbd2b",
+  measurementId: "G-0M7S9NPEEK"
+};
+
+// الحساب الوحيد المسموح له بفتح لوحة المسؤول.
+export const ADMIN_UID = "EE2dpbeCwnSe1Ih7yahI5AhTSK13";
+
+// معرّف الإحصاءات الرئيسي للموقع العام.
+export const SITE_ANALYTICS_ID = "G-NZNC6929YS";
