ead6e2b9f469787c68d238ea91cec57e03246705
track2000one <track2000one@gmail.com>
2026-08-01 16:43:44 +0300

    Configure account question mastery sync

--- app/src/exam-progress-bootstrap.js
+++ app/src/exam-progress-bootstrap.js
@@ -1,10 +1,15 @@
 import { ensureAuth } from './auth.js';
 import { configureProgress } from './progress-client.js';
+import { configureQuestionMastery } from './question-mastery-client.js';
 
 ensureAuth()
   .then(session => {
     configureProgress(session);
-    return window.NEON_PROGRESS?.sync?.();
+    configureQuestionMastery(session);
+    return Promise.allSettled([
+      window.NEON_PROGRESS?.sync?.(),
+      window.NEON_QUESTION_MASTERY?.flush?.()
+    ]);
   })
   .catch(error => {
     console.warn('NEON exam progress bootstrap:', error);
