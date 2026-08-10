75d348e52465264d0deaaa90dd75283fe86ff590
track2000one <track2000one@gmail.com>
2026-08-10 16:18:21 +0300

    Add modern Firebase client config

---
+++ app/src/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCCTXMacLqU0MgHIKi6C7q9VjURAl0srD8',
+  authDomain: 'neon-riddle-2060-admin.firebaseapp.com',
+  projectId: 'neon-riddle-2060-admin',
+  storageBucket: 'neon-riddle-2060-admin.firebasestorage.app',
+  messagingSenderId: '255301604146',
+  appId: '1:255301604146:web:63451bb582f171423cbd2b',
+  measurementId: 'G-0M7S9NPEEK'
+};
+
+export const SITE_ANALYTICS_ID = 'G-NZNC6929YS';
